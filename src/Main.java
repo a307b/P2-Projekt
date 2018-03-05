01ea35c5cee317c9aa04f0459672eaa68e27485f
Seraph2 <frostbyte9@hotmail.com>
2018-03-05 14:28:03 +0100

    Added example Java project

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 // Main Class //
 
-public class Main {
-
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+	{
         System.out.println("test");
     }
 }

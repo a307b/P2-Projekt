a320f7e83cfbf1a0d2dc8e137ca6dece7f515864
Sembrik <peter@servon.eu>
2018-03-15 13:42:24 +0100

    Updated for Server Side only

--- blockchain/src/main/java/Main.java
+++ blockchain/src/main/java/Main.java
@@ -4,14 +4,8 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        //OSCheck oscheck = new OSCheck();
-        IpfsFirewall firewall = new IpfsFirewall("", "" );
-        IpfsCore ipfsCore = new IpfsCore();
-
         OSCheck.updateIPFS();
-
-        System.out.println("Adding you to the whitelisted network... - Your ID is : " + firewall.getPlainIP());
-        firewall.getHostname();
-        //ipfsCore.connect();
+        IpfsCore ipfsCore = new IpfsCore();
+        ipfsCore.connect();
     }
 }
--- blockchain/src/main/java/OSCheck.java
+++ blockchain/src/main/java/OSCheck.java
@@ -3,9 +3,7 @@ public class OSCheck {
     private static String OS = null;
     private static String getOS()
     {
-        if(OS == null)
-            OS = System.getProperty("os.name");
-
+        if(OS == null) { OS = System.getProperty("os.name"); }
         return OS;
     }
     private static boolean isWindows()
@@ -25,13 +23,19 @@ public class OSCheck {
 
     protected static void updateIPFS()
     {
-        if (isWindows())
+        if (isWindows()){
             System.out.println("Detected Windows OS - Installing IPFS for Windows");
-        else if (isLinux())
-            System.out.println("Detected Linux OS - Installing IPFS for Linux");
-        else if (isOSX())
-            System.out.println("Detected OSX OS - Installing IPFS for OSX");
+        }
 
+        else if (isLinux())
+        {
+            System.out.println("Detected Linux OS - Installing IPFS for Linux");
+        }
+
+        else if (isOSX())
+        {
+            System.out.println("Detected OSX OS - Installing IPFS for OSX");
+        }
         System.out.println("OS Version : " + getOS());
     }
 
